045102c1747f76406904f79dba71c05ae5572cb7
Mehmet Altunören <defalt978@gmail.com>
2025-02-24 15:54:56 +0300

    Add files via upload

---
+++ chats.php
@@ -0,0 +1,126 @@
+<?php
+
+    $URL = "https://console.firebase.google.com/u/0/project/cs306-recit-10/database/cs306-recit-10-default-rtdb/data/~2F";
+
+    header("refresh: 60");
+
+    function get_messages() { 
+        global $URL;
+        $ch = curl_init();
+        curl_setopt_array($ch, [ CURLOPT_URL => $URL,
+                                CURLOPT_POST => FALSE, // It will be a get request 
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_SSL_VERIFYPEER => false, ]);
+        $response = curl_exec($ch); 
+        curl_close($ch);
+        return json_decode($response, true); 
+    }
+    
+    function send_msg($msg, $name) { 
+        global $URL;
+        $ch = curl_init();
+        $msg_json = new stdClass();
+        $msg_json->msg = $msg;
+        $msg_json->name = $name;
+        $msg_json->time = date('H:i');
+        $encoded_json_obj = json_encode($msg_json); 
+        curl_setopt_array($ch, array(CURLOPT_URL => $URL,
+                                    CURLOPT_POST => TRUE,
+                                    CURLOPT_RETURNTRANSFER => TRUE,
+                                    CURLOPT_HTTPHEADER => array('Content-Type: application/json' ),
+                                    CURLOPT_POSTFIELDS => $encoded_json_obj ));
+        $response = curl_exec($ch); 
+        return $response;
+    }
+
+    $msg_res_json = get_messages();
+
+    if (isset($_POST['usermsg'])) {
+        $user_msg = $_POST['usermsg'];
+        send_msg($user_msg, "Guest");
+        echo $user_msg;
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="styles.css">
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</head>
+
+<div class="menu">
+<div class="back"><i class="fa fa-chevron-left"></i> <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
+<div class="name">Support</div>
+<div class="last">18:09</div>
+</div>
+<ol class="chat">
+<?php
+    $keys = array_keys($msg_res_json);
+    for ($i = 0; $i < count($keys); $i++){
+        $chat_msg = $msg_res_json[$keys[$i]];
+        $name = $chat_msg['name'];
+        $msg = $chat_msg['msg'];
+        $time = $chat_msg['time'];
+        if ($name == 'admin') {
+            $from = 'other';
+        } else {
+            $from = 'self';
+        }
+       echo  '
+       <li class="'.$from.'">
+       <div class="avatar">
+                <img src="https://i.imgur.com/DY6gND0.png" draggable="false"/>
+            </div>
+                <div class="msg">
+                    <p><b>'.$name.'</b></p>
+                    <p>'.$msg.'</p>
+                    <time>'.$time.'</time>
+                </div>
+            </li>';
+    }
+?>
+</ol>
+<form name="form" action = "chats.php" method="POST">
+    <input name="usermsg" class="textarea" type="text" placeholder="Type here!"/>
+    <input type="submit" style="display: none" />
+</form>
